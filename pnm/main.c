35b70d13929583a1a1a3ddca2a55d9aad83c18d5
Bodanor <papadopoulos.chris@icloud.com>
2021-03-15 02:48:15 +0100

    Initial Commit

---
+++ pnm/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  pnm
+//
+//  Created by Christos Papadopoulos on 15/03/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   printf("Hello, World!\n");
+   return 0;
+}

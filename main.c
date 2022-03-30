7fa0db09d879b1ea04340e8a8fd40992aa7a1508
Bodanor <christos.papadopoulos@student.hepl.be>
2022-03-30 23:25:17 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,100 @@
+/**
+ * main.c
+ * 
+ * Ce fichier contient la fonction main() du programme de manipulation
+ * de fichiers pnm.
+ *
+ * @author: Nom Prenom Matricule
+ * @date: 
+ * @projet: INFO0030 Projet 1
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <getopt.h>
+#include <string.h>
+
+#include "pnm.h"
+
+
+int main(int argc, char *argv[]) {
+
+   char *optstring = "i:o:f:";
+   char *input_path = NULL;
+   char *output_path = NULL;
+   char file_format[4] = {0};
+   int c;
+
+   while ((c = getopt(argc, argv, optstring)) != -1)
+   {
+      switch (c)
+      {
+         case 'i':
+            input_path = (char *)malloc(sizeof(char)*strlen(optarg) + 1);
+            if (input_path == NULL)
+            {
+               printf("Malloc error for input path !\n");
+               return -1;
+            }
+            else
+               strcpy(input_path, optarg);
+            break;
+         case 'o':
+            output_path = (char*)malloc(sizeof(char)*strlen(optarg) + 1);
+            if (output_path == NULL)
+            {
+               printf("Malloc error for output path !\n");
+               if (input_path != NULL)
+                  free(input_path);
+               return -1;
+            }
+            else
+               strcpy(output_path, optarg);
+            break;
+
+         case 'f':
+            strncpy(file_format, optarg, 4);
+            break;
+         
+         case '?':
+               return -1;
+
+         case ':':
+            return -1;
+            break;
+         
+         default:
+            abort();
+
+
+      }
+   }
+
+   if (input_path == NULL)
+   {
+      printf("No input file path !\n");
+      return -1;
+   }
+   else if (output_path == NULL)
+   {
+      printf("No output file path !\n");
+      return -1;
+   }
+   else if (*file_format == '\0')
+   {
+      printf("No file format given !\n");
+      return -1;
+   }
+
+   PNM *image = createPNM(file_format);
+   if (image == NULL)
+      return -1;
+   
+   load_pnm(&image, input_path);
+   //write_pnm(image, output_path);
+   return 0;
+}
+
---
+++ pnm.c
@@ -0,0 +1,306 @@
+/**
+ * pnm.c
+ * 
+ * Ce fichier contient les définitions de types et 
+ * les fonctions de manipulation d'images PNM.
+ * 
+ * @author: Nom Prenom Matricule
+ * @date: 
+ * @projet: INFO0030 Projet 1
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "pnm.h"
+
+
+static char forbidden_char[] = 
+{
+   '/',
+   '\\',
+   ':',
+   '*',
+   '?',
+   '\"',
+   '<',
+   '>',
+   '|',
+   '.'
+};
+
+
+/**
+ * Définition du type opaque PNM
+ *
+ */
+struct PNM_t {
+
+   char file_format[4];
+   char header_format[3];
+   int pixel_intensity;
+   int rows;
+   int columns;
+   int **matrix;
+
+
+};
+
+static FILE *openFile(char *filename)
+{
+   assert(filename != NULL);
+
+   FILE *fp = fopen(filename, "a+b");
+   if (fp == NULL)
+   {
+      printf("Couldn't load file %s\nReason : %s", filename, strerror(errno));
+      return NULL;
+   }
+   else
+      return fp;
+}
+
+static int verify_matching_extension(PNM *image, char *filename)
+{
+   short i;
+   char *ext_pt = filename + (strlen(filename) - 3);
+
+   for (i = 0; i < 3; i++)
+      ext_pt[i] = tolower(ext_pt[i]);
+   
+   if (strcmp(image->file_format, ext_pt) == 0)
+
+      return 0;
+   else
+   {
+      printf("extension passed differs from actual file extension !\n\n");
+      printf("File extension --> %s\n", image->file_format);
+      printf("Extension passed --> %s\n", ext_pt);
+      return -1;
+
+   }   
+   
+}
+
+static int verify_header_format(PNM *image,FILE *input_file)
+{
+   assert(image != NULL);
+   assert(input_file != NULL);
+
+   int c;
+   char *pt_format = image->header_format;
+
+   while ((c = fgetc(input_file)) != '\n' && c != '\r')
+   {
+      if (c == '#')
+         while ((c = fgetc(input_file)) != '\n' && c != '\r')
+            ;
+      else
+         *pt_format++ = c;
+   }
+   *pt_format = '\0';
+   if (strcmp(image->header_format, "P1") != 0 && strcmp(image->header_format, "P2") != 0 && strcmp(image->header_format, "P3") != 0)
+   {
+      printf("Header describing file format is corrupted\n");
+      return -1;
+   }
+
+   if (strcmp(image->header_format, "P1") == 0 && strcmp(image->file_format, "pbm") != 0)
+   {
+      printf("Extension passed differs from the one in the header !\n\n");
+      printf("Header extension --> %s\n", image->header_format);
+      printf("Extension passed --> %s (which must be \"pbm\" for a \"P1\" in the header !)\n", image->file_format);
+      return -2;
+   }
+   else if (strcmp(image->header_format, "P2") == 0 && strcmp(image->file_format, "pgm") != 0)
+   {
+      printf("Extension passed differs from the one in the header !\n\n");
+      printf("Header extension --> %s\n", image->header_format);
+      printf("Extension passed --> %s (which must be \"pgm\" for a \"P2\" in the header !)\n", image->file_format);
+      return -2;
+   }
+   else if (strcmp(image->header_format, "P3") == 0 && strcmp(image->file_format, "ppm") != 0)
+   {
+      printf("Extension passed differs from the one in the header !\n\n");
+      printf("Header extension --> %s\n", image->header_format);
+      printf("Extension passed --> %s (which must be \"ppm\" for a \"P3\" in the header !)\n", image->file_format);
+      return -2;
+   }
+   else
+      return 0;
+
+}
+static int get_matrix_size(PNM *image, FILE *input_file)
+{
+   assert(image != NULL);
+   assert(input_file != NULL);
+
+   int c, i;
+   char columns[10];
+   char rows[10];
+   char *pt_columns= columns;
+   char *pt_rows = rows;
+
+   while ((c = fgetc(input_file)) != ' ')
+   {
+      if (c == '#')
+         while ((c = fgetc(input_file)) != '\n' && c != '\r')
+            ;
+      else
+         *pt_columns++ = c;
+   }
+
+   *pt_columns= '\0';
+
+   while ((c = fgetc(input_file)) != '\n' && c != '\r')
+   {
+      if (c == '#')
+         while ((c = fgetc(input_file)) != '\n' && c != '\r')
+            ;
+      else
+         *pt_rows++ = c;
+   }
+   *pt_rows = '\0';
+
+   for (i = 0; i < (int)strlen(rows); i++)
+      if (isdigit(rows[i]) == 0)
+      {
+         printf("Columns numbers in the header file is corrupted. Non digit char detected !\n");
+         return -1;
+      }
+   
+   image->columns = atoi(columns);
+   for (i = 0; i < (int)strlen(rows); i++)
+      if (isdigit(rows[i]) == 0)
+         {
+            printf("Rows numbers in the header file is corrupted. Non digit char detected !\n");
+            return -1;
+         }
+
+
+   image->rows = atoi(rows);
+   return 0;
+}
+
+static int get_pixel_intensity(PNM *image, FILE *input_file)
+{
+   assert(image != NULL);
+   assert(input_file != NULL);
+
+   int c, i;
+   char pixel_intensity[10];
+   char *pt_pixel = pixel_intensity;
+
+   while ((c = fgetc(input_file)) != '\n' && c != '\r')
+   {
+      if (c == '#')
+         while ((c = fgetc(input_file)) != '\n' && c != '\r')
+            ;
+      else
+         *pt_pixel++ = c;
+   }
+
+   for (i = 0; i < (int)strlen(pixel_intensity); i++)
+      if (isdigit(pixel_intensity[i]) == 0)
+      {
+         printf("Pixel intensity in the header file is corrupted. Non digit char detected !\n");
+         return -1;
+      }
+   
+   image->pixel_intensity = atoi(pixel_intensity);
+   return 0;
+
+
+}
+static int allocate_matrix(PNM *image)
+{
+   assert(image != NULL);
+
+   int i;
+
+   image->matrix = (int **)malloc(sizeof(int*)*image->rows);
+   if (image->matrix == NULL)
+      return -1;
+
+   for (i = 0; i < image->rows; i++)
+   {
+      *(image->matrix + i) = (int*)malloc(image->columns * sizeof(int));
+      if (*(image->matrix + i) == NULL)
+         return -1;
+   }
+   
+   return 0;
+
+}
+int load_pnm(PNM **image, char* filename) {
+
+   assert(*image != NULL);
+   assert (filename != NULL);
+
+   FILE *input_file = openFile(filename);
+   if (input_file == NULL)
+      return -1;
+   
+   if (verify_matching_extension(*image, filename) == -1)
+      return -2;
+
+   printf("Loading data from \"%s\"...\n", filename);
+
+   if(verify_header_format(*image, input_file) == -1)
+      return -3;
+
+   if (get_matrix_size(*image, input_file) == -1)
+      return -1;
+   
+   if (allocate_matrix(*image) == -1)
+      return -1;
+   
+   if (strcmp((*image)->header_format, "P2") == 0 || strcmp((*image)->header_format, "P3") == 0)
+      if (get_pixel_intensity(*image, input_file) == -1)
+         return -1;
+   return 0;
+}
+
+int write_pnm(PNM *image, char* filename) {
+
+   FILE *output = openFile(filename);
+   int i, x;
+   srand(time(NULL));
+
+   for (i = 0; i < image->rows ; i++)
+      for (x = 0; x < image->columns ; x++)
+         fprintf(output, "%d ", *(*(image->matrix + i) + x));
+   
+   return 0;
+}
+
+PNM *createPNM(char *filetype)
+{
+   PNM *image = (PNM*)malloc(sizeof(PNM));
+   if (image == NULL)
+      printf("Malloc error !\n");
+   
+   strcpy(image->file_format, filetype);
+   return image;
+
+}
+int verify_forbidden_input_char(char *string)
+{
+   assert(string != NULL);
+
+   size_t i;
+   size_t x;
+   char *str_pt = string;
+
+   for (i = 0; i < strlen(string); i++)
+   {
+      for (x = 0; x < sizeof(forbidden_char); x++)
+      {
+         if (*str_pt == forbidden_char[x])
+            return -1;
+      }
+      str_pt++;
+   }
+   return 0;
+}
